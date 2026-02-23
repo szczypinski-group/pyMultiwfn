b33798b6d8aa31a5c27ffe3ef87bc30794283a7f
VALERO <ypkh17@durham.ac.uk>
2026-02-23 09:56:28 +0000

    feat: added extended convinience functionality to better match updated menu

--- src/pymultiwfn/analysis.py
+++ src/pymultiwfn/analysis.py
@@ -56,12 +56,18 @@ class MultiwfnAnalysis:
     >>> all_results = analysis.run_all()
     """
 
+    # =========================================================================
     # Category definitions
+    # =========================================================================
+
     CHARGES = [
         Menu.HIRSHFELD_CHARGE,
         Menu.VDD_POPULATION,
         Menu.MULLIKEN_POPULATION,
         Menu.LOWDIN_POPULATION,
+        Menu.SCPA_POPULATION,
+        Menu.STOUT_POLITZER_POPULATION,
+        Menu.BICKELHAUPT_POPULATION,
         Menu.BECKE_CHARGE,
         Menu.ADCH_CHARGE,
         Menu.CHELPG_CHARGE,
@@ -73,29 +79,50 @@ class MultiwfnAnalysis:
         Menu.RESP_CHARGE,
         Menu.GASTEIGER_CHARGE,
         Menu.MBIS_CHARGE,
+        Menu.DDEC_CHARGE,
     ]
 
     BOND_ORDERS = [
         Menu.MAYER_BOND_ORDER,
         Menu.MULTICENTER_BOND_ORDER,
+        Menu.MULTICENTER_BOND_ORDER_NAO,
         Menu.WIBERG_BOND_ORDER,
         Menu.MULLIKEN_BOND_ORDER,
+        Menu.MULLIKEN_BOND_ORDER_DECOMPOSE,
+        Menu.ORBITAL_PERTURBED_MAYER,
         Menu.FUZZY_BOND_ORDER,
         Menu.LAPLACIAN_BOND_ORDER,
+        Menu.WIBERG_DECOMPOSITION,
+        Menu.IBSI_ANALYSIS,
+        Menu.AV1245_INDEX,
     ]
 
     TOPOLOGY = [
         Menu.TOPOLOGY_SEARCH_CPS,
         Menu.TOPOLOGY_GENERATE_PATHS,
+        Menu.TOPOLOGY_INTERBASIN_SURFACES,
         Menu.TOPOLOGY_ANALYSIS_COMPLETE,
+        Menu.TOPOLOGY_ESP_ANALYSIS,
+        Menu.TOPOLOGY_LOL_ANALYSIS,
+        Menu.TOPOLOGY_ELF_ANALYSIS,
+        Menu.TOPOLOGY_LAPLACIAN_ANALYSIS,
+        Menu.TOPOLOGY_SEARCH_BCP,
+        Menu.TOPOLOGY_SEARCH_RCP,
+        Menu.TOPOLOGY_SEARCH_CCP,
     ]
 
     WEAK_INTERACTIONS = [
         Menu.NCI_ANALYSIS,
+        Menu.NCI_PROMOLECULAR,
+        Menu.ANCI_ANALYSIS,
         Menu.IRI_ANALYSIS,
         Menu.DORI_ANALYSIS,
+        Menu.VDW_POTENTIAL,
         Menu.IGM_ANALYSIS,
         Menu.IGMH_ANALYSIS,
+        Menu.AIGM_ANALYSIS,
+        Menu.MIGM_ANALYSIS,
+        Menu.AMIGM_ANALYSIS,
     ]
 
     SPECTRA = [
@@ -106,39 +133,73 @@ class MultiwfnAnalysis:
         Menu.PLOT_VCD_SPECTRUM,
         Menu.PLOT_ROA_SPECTRUM,
         Menu.PLOT_NMR_SPECTRUM,
+        Menu.PLOT_FLUORESCENCE_SPECTRUM,
+        Menu.PLOT_PVS,
+        Menu.PREDICT_COLOR,
     ]
 
     SURFACES = [
         Menu.SURFACE_ANALYSIS_ESP,
         Menu.SURFACE_ANALYSIS_ALIE,
         Menu.SURFACE_AREA_VOLUME,
+        Menu.BECKE_SURFACE,
+        Menu.HIRSHFELD_SURFACE,
+        Menu.SURFACE_EXTREMA,
+        Menu.HIRSHFELD_SURFACE_FINGERPRINT,
     ]
 
     AROMATICITY = [
+        Menu.AICD_ANALYSIS,
+        Menu.NICS_POINT,
         Menu.ICSS_ANALYSIS,
         Menu.NICS_SCAN,
+        Menu.BIRD_INDEX,
         Menu.HOMA_INDEX,
+        Menu.HOMAC_HOMER,
+        Menu.STANGER_INDEX,
+        Menu.NICS_1D_SCAN,
+        Menu.NICS_2D_MAP,
     ]
 
     CDFT = [
+        Menu.CDFT_ANALYSIS,
         Menu.FUKUI_FUNCTION,
         Menu.DUAL_DESCRIPTOR,
         Menu.CONDENSED_FUKUI,
+        Menu.LOCAL_HARDNESS,
+        Menu.LOCAL_IONIZATION_ENERGY,
     ]
 
     DOS = [
         Menu.PLOT_DOS,
         Menu.PLOT_PDOS,
+        Menu.PLOT_OPDOS,
+        Menu.PLOT_LDOS,
+        Menu.PLOT_PHOTOELECTRON_SPECTRUM,
+        Menu.PLOT_COHP,
     ]
 
     BASIN = [
         Menu.BASIN_ANALYSIS_AIM,
         Menu.BASIN_ANALYSIS_ELF,
+        Menu.BASIN_INTEGRATE_PROPERTY,
+        Menu.BASIN_ANALYSIS_ESP,
+        Menu.BASIN_ANALYSIS_LOL,
+        Menu.BASIN_ANALYSIS_LOL_ALPHA,
+        Menu.BASIN_ANALYSIS_CUSTOM,
     ]
 
     EXCITATION = [
         Menu.HOLE_ELECTRON_ANALYSIS,
+        Menu.TRANSITION_DENSITY_MATRIX,
         Menu.CHARGE_TRANSFER_ANALYSIS,
+        Menu.DELTA_R_INDEX,
+        Menu.TRANSITION_DIPOLE_MOMENTS,
+        Menu.GENERATE_NTO,
+        Menu.IFCT_ANALYSIS,
+        Menu.LAMBDA_INDEX,
+        Menu.CTS_ANALYSIS,
+        Menu.CONDITIONAL_DENSITY,
     ]
 
     CUBES = [
@@ -148,6 +209,120 @@ class MultiwfnAnalysis:
         Menu.CUBE_LOL,
         Menu.CUBE_ESP,
         Menu.CUBE_LAPLACIAN,
+        Menu.CUBE_GRADIENT_NORM,
+        Menu.CUBE_KINETIC_G,
+        Menu.CUBE_KINETIC_K,
+        Menu.CUBE_ALIE,
+        Menu.CUBE_RDG,
+        Menu.CUBE_SIGN_LAMBDA2_RHO,
+        Menu.CUBE_SOURCE_FUNCTION,
+        Menu.CUBE_ORBITAL_WAVEFUNCTION,
+        Menu.CUBE_FUKUI_MINUS,
+        Menu.CUBE_FUKUI_PLUS,
+        Menu.CUBE_DUAL_DESCRIPTOR,
+        Menu.CUBE_PROMOLECULAR_DENSITY,
+        Menu.CUBE_DEFORMATION_DENSITY,
+        Menu.CUBE_DENSITY_HIGH,
+        Menu.CUBE_ESP_HIGH,
+        Menu.CUBE_ELF_HIGH,
+    ]
+
+    ORBITAL_COMPOSITION = [
+        Menu.ORBITAL_COMPOSITION_MULLIKEN,
+        Menu.ORBITAL_COMPOSITION_SCPA,
+        Menu.ORBITAL_COMPOSITION_STOUT_POLITZER,
+        Menu.ORBITAL_COMPOSITION_FRAGMENT_MULLIKEN,
+        Menu.ORBITAL_COMPOSITION_FRAGMENT_STOUT,
+        Menu.ORBITAL_COMPOSITION_FRAGMENT_SCPA,
+        Menu.ORBITAL_COMPOSITION_NAO,
+        Menu.ORBITAL_COMPOSITION_HIRSHFELD,
+        Menu.ORBITAL_COMPOSITION_BECKE,
+        Menu.LOBA_OXIDATION_STATE,
+    ]
+
+    ORBITAL_LOCALIZATION = [
+        Menu.BOYS_LOCALIZATION,
+        Menu.PIPEK_MEZEY_LOCALIZATION,
+    ]
+
+    FUZZY_SPACE = [
+        Menu.FUZZY_INTEGRATE_PROPERTY,
+        Menu.ATOMIC_DIPOLE_MOMENTS,
+        Menu.ATOMIC_OVERLAP_MATRIX,
+        Menu.LOCALIZATION_DELOCALIZATION_INDEX,
+        Menu.PDI_AROMATICITY,
+        Menu.FLU_AROMATICITY,
+        Menu.FLU_PI_AROMATICITY,
+        Menu.FUZZY_INTEGRATE_OVERLAP,
+        Menu.CONDENSED_LINEAR_RESPONSE,
+        Menu.PARA_LINEAR_RESPONSE,
+        Menu.MULTICENTER_DI,
+        Menu.ITA_AROMATICITY,
+        Menu.ATOMIC_VOLUME_POLARIZABILITY,
+        Menu.IFDI_ANALYSIS,
+    ]
+
+    EDA = [
+        Menu.EDA_FF,
+        Menu.EDA_SBL,
+        Menu.SOBEDA_ANALYSIS,
+        Menu.DISPERSION_ATOMIC_CONTRIBUTION,
+    ]
+
+    POLARIZABILITY = [
+        Menu.PARSE_POLARIZABILITY,
+        Menu.SOS_POLARIZABILITY,
+        Menu.POLARIZABILITY_DENSITY,
+        Menu.UNIT_SPHERE_POLARIZABILITY,
+    ]
+
+    WAVEFUNCTION = [
+        Menu.CHECK_WAVEFUNCTION,
+        Menu.PRINT_ORBITAL_INFO,
+        Menu.PRINT_GTF_INFO,
+        Menu.PRINT_BASIS_INFO,
+        Menu.PRINT_DENSITY_MATRIX,
+        Menu.PRINT_OVERLAP_MATRIX,
+        Menu.MODIFY_OCCUPATION,
+        Menu.SAVE_WAVEFUNCTION_WFN,
+        Menu.DELETE_INNER_ORBITALS,
+    ]
+
+    LINE_PLOTS = [
+        Menu.LINE_ESP,
+        Menu.LINE_ELECTRON_DENSITY,
+        Menu.LINE_LAPLACIAN,
+        Menu.LINE_ELF,
+        Menu.LINE_LOL,
+        Menu.LINE_RDG,
+        Menu.LINE_SPIN_DENSITY,
+        Menu.LINE_GRADIENT_NORM,
+        Menu.LINE_KINETIC_G,
+        Menu.LINE_KINETIC_K,
+        Menu.LINE_ALIE,
+        Menu.LINE_SOURCE_FUNCTION,
+    ]
+
+    PLANE_MAPS = [
+        Menu.PLANE_MAP_DENSITY,
+        Menu.PLANE_MAP_ESP,
+        Menu.PLANE_MAP_ELF,
+        Menu.PLANE_MAP_LOL,
+        Menu.PLANE_MAP_GRADIENT,
+        Menu.PLANE_MAP_LAPLACIAN,
+        Menu.PLANE_MAP_SPIN_DENSITY,
+        Menu.PLANE_MAP_RDG,
+        Menu.PLANE_MAP_SIGN_LAMBDA2_RHO,
+        Menu.PLANE_MAP_ALIE,
+        Menu.PLANE_MAP_KINETIC_G,
+        Menu.PLANE_MAP_KINETIC_K,
+        Menu.PLANE_MAP_SOURCE_FUNCTION,
+        Menu.PLANE_MAP_ORBITAL_WAVEFUNCTION,
+        Menu.PLANE_MAP_FUKUI_MINUS,
+        Menu.PLANE_MAP_FUKUI_PLUS,
+        Menu.PLANE_MAP_DUAL_DESCRIPTOR,
+        Menu.PLANE_MAP_DEFORMATION_DENSITY,
+        Menu.PLANE_MAP_PROMOLECULAR_DENSITY,
     ]
 
     CATEGORIES: dict[str, list[Menu]] = {
@@ -163,6 +338,14 @@ class MultiwfnAnalysis:
         "basin": BASIN,
         "excitation": EXCITATION,
         "cubes": CUBES,
+        "orbital_composition": ORBITAL_COMPOSITION,
+        "orbital_localization": ORBITAL_LOCALIZATION,
+        "fuzzy_space": FUZZY_SPACE,
+        "eda": EDA,
+        "polarizability": POLARIZABILITY,
+        "wavefunction": WAVEFUNCTION,
+        "line_plots": LINE_PLOTS,
+        "plane_maps": PLANE_MAPS,
     }
 
     def __init__(
@@ -305,9 +488,69 @@ class MultiwfnAnalysis:
         """Run all CDFT analyses."""
         return self.run_batch(self.CDFT, verbose)
 
+    def run_dos(self, verbose: bool = False) -> dict[str, MultiwfnResult]:
+        """Run all density of states analyses."""
+        return self.run_batch(self.DOS, verbose)
+
+    def run_basin(self, verbose: bool = False) -> dict[str, MultiwfnResult]:
+        """Run all basin analyses."""
+        return self.run_batch(self.BASIN, verbose)
+
+    def run_excitation(
+        self, verbose: bool = False
+    ) -> dict[str, MultiwfnResult]:
+        """Run all electron excitation analyses."""
+        return self.run_batch(self.EXCITATION, verbose)
+
     def run_cubes(self, verbose: bool = False) -> dict[str, MultiwfnResult]:
         """Run all cube generation analyses."""
         return self.run_batch(self.CUBES, verbose)
+
+    def run_orbital_composition(
+        self, verbose: bool = False
+    ) -> dict[str, MultiwfnResult]:
+        """Run all orbital composition analyses."""
+        return self.run_batch(self.ORBITAL_COMPOSITION, verbose)
+
+    def run_orbital_localization(
+        self, verbose: bool = False
+    ) -> dict[str, MultiwfnResult]:
+        """Run all orbital localization analyses."""
+        return self.run_batch(self.ORBITAL_LOCALIZATION, verbose)
+
+    def run_fuzzy_space(
+        self, verbose: bool = False
+    ) -> dict[str, MultiwfnResult]:
+        """Run all fuzzy atomic space analyses."""
+        return self.run_batch(self.FUZZY_SPACE, verbose)
+
+    def run_eda(self, verbose: bool = False) -> dict[str, MultiwfnResult]:
+        """Run all energy decomposition analyses."""
+        return self.run_batch(self.EDA, verbose)
+
+    def run_polarizability(
+        self, verbose: bool = False
+    ) -> dict[str, MultiwfnResult]:
+        """Run all polarizability analyses."""
+        return self.run_batch(self.POLARIZABILITY, verbose)
+
+    def run_wavefunction(
+        self, verbose: bool = False
+    ) -> dict[str, MultiwfnResult]:
+        """Run all wavefunction check/modify analyses."""
+        return self.run_batch(self.WAVEFUNCTION, verbose)
+
+    def run_line_plots(
+        self, verbose: bool = False
+    ) -> dict[str, MultiwfnResult]:
+        """Run all line property plot analyses."""
+        return self.run_batch(self.LINE_PLOTS, verbose)
+
+    def run_plane_maps(
+        self, verbose: bool = False
+    ) -> dict[str, MultiwfnResult]:
+        """Run all plane property map analyses."""
+        return self.run_batch(self.PLANE_MAPS, verbose)
 
     def run_category(
         self,
