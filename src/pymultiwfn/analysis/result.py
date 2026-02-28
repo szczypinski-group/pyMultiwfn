60138c01e9a9bac1e3fe96ad7c6f996dcf0b550f
Filip Szczypiński <fiszczyp@gmail.com>
2026-02-28 21:53:47 +0000

    fix: import parsers

--- src/pymultiwfn/analysis/result.py
+++ src/pymultiwfn/analysis/result.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
-from pymultiwfn.parsers import (
+from pymultiwfn.interface.parsers import (
     BondOrderParser,
     ChargeParser,
     CriticalPointParser,
